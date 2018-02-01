550d1ec650410b5593a3bc79cad96c3795d5e130
princesaoud <kanartyoussouf@gmail.com>
2018-02-02 03:54:56 +0530

    update config/app.php & welcome and web

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/welcome');
+});
+
+Route::get('/about', function(){
+  return view('pages/about');
+});
+
+Route::get('/help', function(){
+  return view('pages/help');
 });
